2089fa27e07f737f934f07ecc8733d8e4affc391
BigBen3918 <benjamin199551@gmail.com>
2022-08-02 09:28:32 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useRoutes } from 'react-router-dom';
+import router from 'src/router';
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+import { CssBaseline } from '@mui/material';
+import ThemeProvider from './theme/ThemeProvider';
+
+function App() {
+  const content = useRoutes(router);
+
+  return (
+    <ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <CssBaseline />
+        {content}
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+}
+export default App;
